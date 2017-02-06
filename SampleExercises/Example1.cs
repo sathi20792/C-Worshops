2c5e3e13b1c7cf59ed73f76abe6da62be4588320
User <User@DESKTOP-1M8ETEI>
2017-02-06 13:44:43 +0800

    Add project files.

---
+++ SampleExercises/Example1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SampleExercises
+{
+    class Example1
+    {
+    static void Main(string[] args)
+        {
+
+        }
+}
---
+++ SampleExercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // excercise 1
+            Console.WriteLine("****************EXERCISE 1************");
+            Console.WriteLine("Sathish Manivannan");
+            Console.WriteLine("sathishmanivannan20@gmail.com\n");
+
+            // exercise 2
+            Console.WriteLine("****************EXERCISE 2************");
+            Console.WriteLine("Please enter your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Good Morning " + name+"\n");
+
+            // exercise 3
+            Console.WriteLine("****************EXERCISE 3************");
+            Console.WriteLine("Please enter a number");
+            int num=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("The Square of the number is " + num * num+"\n");
+
+            //exercise 4
+            Console.WriteLine("****************EXERCISE 4************");
+            Console.WriteLine("Please enter a double preision number");
+            double dnum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The Square of the double precision number is " + dnum * dnum + "\n");
+
+            //exercise 5
+            Console.WriteLine("****************EXERCISE 5************");
+            Console.WriteLine("Please enter a single precision number");
+            double dnum2 = Convert.ToDouble(Console.ReadLine());
+            double square = dnum2 * dnum2;
+            string number= String.Format("{0:0.00}", square);
+            Console.WriteLine("The Square of the single precision number is "+number);
+            Console.ReadLine();
+
+
+        }
+    }
+}
